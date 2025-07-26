3259c56eacea8f53ae455d35fb8d6071ca6064cf
kwr011024 <kwr011024@163.com>
2025-07-26 23:45:01 +0800

    build(deps): 升级 vue 版本并简化 vite 配置
    - 将 vue 版本从 3.5.13 升级到 3.5.18
    - 移除了 vite.config.ts 中的冗余配置项，简化配置结构

--- vite.config.ts
+++ vite.config.ts
@@ -25,51 +25,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    // css代码分割
-    cssCodeSplit: true,
-    // 启用最大程度代码分割
-    minify: 'terser',
-    terserOptions: {
-      // 生产环境时移除console
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      },
-      format: {
-        // 移除注释
-        comments: false
-      }
-    },
-    // 编译目标
-    target: 'es2015',
-    // 禁用源映射
-    sourcemap: false,
-    // 控制文件名中的哈希值
-    rollupOptions: {
-      output: {
-        // 代码分割策略
-        manualChunks: id => {
-          if (id.includes('node_modules')) {
-            if (id.includes('vue') || id.includes('vue-router')) {
-              return 'vendor/vue'
-            }
-            if (id.includes('axios')) {
-              return 'vendor/axios'
-            }
-            if (id.includes('wujie')) {
-              return 'vendor/wujie'
-            }
-            return 'vendor/index'
-          }
-          return id.split('src/').pop()
-        },
-        // 文件命名规则
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash][extname]'
-      }
-    }
   }
 })
