91f09be07584ae383f07ed2e70d85c73f8aa5933
even <kwr011024@163.com>
2025-07-26 10:55:02 +0800

    feat(login): 添加登录功能
    - 在 login-view.vue 中添加登录表单的双向绑定
    - 新增 login 方法处理表单提交
    - 引入 Vue 的 ref 函数用于响应式表单数据

--- src/views/login/login-view.vue
+++ src/views/login/login-view.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="loginPage" class="login-page">
+  <div class="login-page">
     <div class="login-container">
       <div class="login-header">
         <h1>HUSH CLUB</h1>
         <p>管理后台登录</p>
       </div>
-      <form id="loginForm" class="login-form">
+      <form id="loginForm" class="login-form" @submit.prevent="login">
         <div class="form-group">
           <label>用户名</label>
-          <input id="loginUsername" type="text" class="form-control" required />
+          <input v-model="loginForm.username" type="text" class="form-control" required />
         </div>
         <div class="form-group">
           <label>密码</label>
-          <input id="loginPassword" type="password" class="form-control" required />
+          <input v-model="loginForm.password" type="password" class="form-control" required />
         </div>
         <button type="submit" class="btn btn-primary login-btn">登录</button>
       </form>
@@ -22,3 +22,16 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const loginForm = ref({
+  username: '',
+  password: ''
+})
+
+const login = () => {
+  console.log(loginForm.value)
+}
+</script>
